209af2bc5e8b0311fbb969ac580304695dda6088
Peter Hall <phall@odecee.com.au>
2015-03-18 16:55:03 +1100

    formatting clean up of setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ from setuptools import setup, find_packages
 from glob import glob
 
 setup(
-        name = "cumulus",
-        version = "0.3a0",
-        author = "Peter Hall",
-        author_email= 'peter.hall@catchoftheday.com.au',
-        license='Apache Software License 2.0',
-        description='Manages AWS Cloudformation stacks across multiple CF templates',
-        packages = find_packages(),
-        include_package_data = True,
-        install_requires=['PyYAML', 'argparse', 'boto', 'simplejson','pystache'],
-        scripts=glob('bin/*'),
+    name="cumulus",
+    version="0.3a0",
+    author="Peter Hall",
+    author_email='cumulus@peterkh.net',
+    license='Apache Software License 2.0',
+    description='Manages AWS Cloudformation stacks across multiple CF'
+                ' templates',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['PyYAML', 'argparse', 'boto', 'simplejson', 'pystache'],
+    scripts=glob('bin/*'),
 )
