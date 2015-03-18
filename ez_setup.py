49dad75cc58325189e89b0f73d815ab222cd3b8a
Ash Caire <ash.caire@gmail.com>
2015-03-18 17:50:38 +1100

    pep8

--- ez_setup.py
+++ ez_setup.py
@@ -33,9 +33,11 @@ except ImportError:
 DEFAULT_VERSION = "2.0.1"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
+
 def _python_cmd(*args):
     args = (sys.executable,) + args
     return subprocess.call(args) == 0
+
 
 def _install(tarball, install_args=()):
     # extracting the tarball
@@ -129,9 +131,9 @@ def use_setuptools(version=DEFAULT_VERSION, download_base=DEFAULT_URL,
     except pkg_resources.VersionConflict as VC_err:
         if imported:
             msg = textwrap.dedent("""
-                The required version of setuptools (>={version}) is not available,
-                and can't be installed while this script is running. Please
-                install a more recent version first, using
+                The required version of setuptools (>={version}) is not
+                available, and can't be installed while this script is
+                running. Please install a more recent version first, using
                 'easy_install -U setuptools'.
 
                 (Currently using {VC_err.args[0]!r})
@@ -142,6 +144,7 @@ def use_setuptools(version=DEFAULT_VERSION, download_base=DEFAULT_URL,
         # otherwise, reload ok
         del pkg_resources, sys.modules['pkg_resources']
         return _do_download(version, download_base, to_dir, download_delay)
+
 
 def _clean_check(cmd, target):
     """
@@ -155,6 +158,7 @@ def _clean_check(cmd, target):
             os.unlink(target)
         raise
 
+
 def download_file_powershell(url, target):
     """
     Download the file at url to target using Powershell (which will validate
@@ -164,9 +168,11 @@ def download_file_powershell(url, target):
     cmd = [
         'powershell',
         '-Command',
-        "(new-object System.Net.WebClient).DownloadFile(%(url)r, %(target)r)" % vars(),
+        "(new-object System.Net.WebClient).DownloadFile(%(url)r,"
+        " %(target)r)" % vars(),
     ]
     _clean_check(cmd, target)
+
 
 def has_powershell():
     if platform.system() != 'Windows':
@@ -184,9 +190,11 @@ def has_powershell():
 
 download_file_powershell.viable = has_powershell
 
+
 def download_file_curl(url, target):
     cmd = ['curl', url, '--silent', '--output', target]
     _clean_check(cmd, target)
+
 
 def has_curl():
     cmd = ['curl', '--version']
@@ -202,9 +210,11 @@ def has_curl():
 
 download_file_curl.viable = has_curl
 
+
 def download_file_wget(url, target):
     cmd = ['wget', url, '--quiet', '--output-document', target]
     _clean_check(cmd, target)
+
 
 def has_wget():
     cmd = ['wget', '--version']
@@ -219,6 +229,7 @@ def has_wget():
     return True
 
 download_file_wget.viable = has_wget
+
 
 def download_file_insecure(url, target):
     """
@@ -245,6 +256,7 @@ def download_file_insecure(url, target):
 
 download_file_insecure.viable = lambda: True
 
+
 def get_best_downloader():
     downloaders = [
         download_file_powershell,
@@ -256,6 +268,7 @@ def get_best_downloader():
     for dl in downloaders:
         if dl.viable():
             return dl
+
 
 def download_setuptools(version=DEFAULT_VERSION, download_base=DEFAULT_URL,
                         to_dir=os.curdir, delay=15,
@@ -329,6 +342,7 @@ def _build_install_args(options):
     """
     return ['--user'] if options.user_install else []
 
+
 def _parse_args():
     """
     Parse the command line for options
@@ -350,11 +364,13 @@ def _parse_args():
     # positional arguments are ignored
     return options
 
+
 def main(version=DEFAULT_VERSION):
     """Install or upgrade setuptools and EasyInstall"""
     options = _parse_args()
     tarball = download_setuptools(download_base=options.download_base,
-        downloader_factory=options.downloader_factory)
+                                  downloader_factory=options.downloader_factory
+                                  )
     return _install(tarball, _build_install_args(options))
 
 if __name__ == '__main__':
